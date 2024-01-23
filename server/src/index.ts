4670ffa7f896ccf28ccfcc061c40b54fa8cf246c
Valentin RAGOT <valentin.ragot2001@gmail.com>
2024-01-23 19:16:18 +0100

    Implement Socket.IO

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,31 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import {createServer} from "node:http";
+import {Server, Socket} from "socket.io";
 import dotenv from 'dotenv';
 dotenv.config();
 import routes from './routes';
 import cors from 'cors';
+import {isLogged} from "./middleware/websocket/isLogged.middleware";
 
 const app = express();
+
+const httpServer = createServer(app);
 app.use(express.json());
 app.use(cors());
 app.use("/api", routes);
 
-app.listen(process.env.APP_PORT, () => {
+const io = new Server(httpServer);
+io.engine.use(cors());
+io.use(async (socket: Socket, next) => { isLogged(socket, next); });
+io.on("connection", (socket: Socket) => {
+    const user: any = socket.handshake.query.user;
+    console.log("Socket connected: " + socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("Socket disconnected: " + socket.id);
+    });
+});
+
+httpServer.listen(process.env.APP_PORT, () => {
     console.log(`Server running at http://localhost:${process.env.APP_PORT}`);
 });
