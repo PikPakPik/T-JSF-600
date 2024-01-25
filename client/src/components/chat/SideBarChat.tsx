e9009cc535ea0409d89c92da4fe402a0dc9a080c
Alexandre.T <alexandre.tressel@icloud.com>
2024-01-25 17:02:29 +0100

    style: :sparkles: Import settings icon with lucide-react instead svg

--- client/src/components/chat/SideBarChat.tsx
+++ client/src/components/chat/SideBarChat.tsx
@@ -1,10 +1,10 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Button } from "@/components/ui/button";
 import { Room } from "@/types/Room";
-import { SVGProps, useEffect, useState } from "react";
+import { Settings } from "lucide-react";
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Link } from "react-router-dom";
-import { JSX } from "react/jsx-runtime";
 
 export default function SideBarChat() {
   const [roomsDefault, setRoomsDefault] = useState<Room[]>([]);
@@ -42,7 +42,7 @@ export default function SideBarChat() {
           <h2 className="text-lg font-semibold dark:text-white">John Doe</h2>
         </div>
         <Button size="icon" variant="outline" className="dark:text-white">
-          <SettingsIcon className="w-5 h-5" />
+          <Settings className="w-5 h-5" />
         </Button>
       </div>
       <div className="flex-1 overflow-y-auto">
@@ -107,27 +107,5 @@ export default function SideBarChat() {
         </div>
       </div>
     </div>
-  );
-}
-
-function SettingsIcon(
-  props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>
-) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z" />
-      <circle cx="12" cy="12" r="3" />
-    </svg>
   );
 }
