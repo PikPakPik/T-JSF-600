8baa47511d043d3d7f95c548da5f3fc688a248f4
Alexandre.T <alexandre.tressel@icloud.com>
2024-01-25 12:11:37 +0100

    feat: :bricks: Add localized dates

--- client/src/utils/ISOToRelativeTime.ts
+++ client/src/utils/ISOToRelativeTime.ts
@@ -1,22 +1,10 @@
-export function ISOToRelativeTime(isoDate: string): string {
-  const date = new Date(isoDate);
-  const now = new Date();
-  const diff = now.getTime() - date.getTime();
-  const minutes = Math.floor(diff / (1000 * 60));
-  const hours = Math.floor(diff / (1000 * 60 * 60));
-  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-  const months = Math.floor(diff / (1000 * 60 * 60 * 24 * 30));
-  const years = Math.floor(diff / (1000 * 60 * 60 * 24 * 30 * 12));
+import "moment/locale/fr";
+import moment from "moment/min/moment-with-locales";
+import { useTranslation } from "react-i18next";
 
-  if (minutes < 60) {
-    return `Il y a ${minutes} minutes`;
-  } else if (hours < 24) {
-    return `Il y a ${hours} heures`;
-  } else if (days < 30) {
-    return `Il y a ${days} jours`;
-  } else if (months < 12) {
-    return `Il y a ${months} mois`;
-  } else {
-    return `Il y a ${years} ans`;
-  }
+export function ISOToRelativeTime(isoDate: string): string {
+  const { i18n } = useTranslation();
+  moment.locale(i18n.language === "gb" ? "en-gb" : i18n.language);
+  const mmnt = moment(isoDate);
+  return mmnt.fromNow();
 }
