357589166b745294d5562d6532b3170459ef5e19
Alexandre.T <alexandre.tressel@icloud.com>
2024-01-25 11:24:23 +0100

    fix: :bug: Add months date for ISOToRelativeTime

--- client/src/utils/ISOToRelativeTime.ts
+++ client/src/utils/ISOToRelativeTime.ts
@@ -5,12 +5,18 @@ export function ISOToRelativeTime(isoDate: string): string {
   const minutes = Math.floor(diff / (1000 * 60));
   const hours = Math.floor(diff / (1000 * 60 * 60));
   const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const months = Math.floor(diff / (1000 * 60 * 60 * 24 * 30));
+  const years = Math.floor(diff / (1000 * 60 * 60 * 24 * 30 * 12));
 
   if (minutes < 60) {
     return `Il y a ${minutes} minutes`;
   } else if (hours < 24) {
     return `Il y a ${hours} heures`;
-  } else {
+  } else if (days < 30) {
     return `Il y a ${days} jours`;
+  } else if (months < 12) {
+    return `Il y a ${months} mois`;
+  } else {
+    return `Il y a ${years} ans`;
   }
 }
